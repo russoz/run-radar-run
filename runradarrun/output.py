132346a7209684c8d7b472e2f2b9a76b54489c19
Alexei Znamensky <russoz@gmail.com>
2025-01-12 21:57:57 +1300

    simplify Printer.logger() even further

--- runradarrun/output.py
+++ runradarrun/output.py
@@ -36,18 +36,14 @@ class Printer:
             log_lines = []
             for line in stream:
                 line = line.decode("utf-8").strip()
-                for line1 in self.term.wrap(line) or [""]:
-                    log_lines.append(line1)
+                for wrapped_line in self.term.wrap(line) or [""]:
+                    log_lines.append(wrapped_line)
 
                 log_lines = log_lines[-log_height:]
                 log_lines[-1] = log_lines[-1] + self.term.clear_eol + "\n"
                 with self.term.location(0, start_row):
                     for log_line in log_lines:
-                        self.print(
-                            (f"{log_line}"),
-                            end="",
-                            flush=True,
-                        )
+                        self.print(f"{log_line}", end="", flush=True)
 
                 if trigger:
                     trigger(line)
