40198d8068a523cb1cf383b857175811b77a6410
semantic-release <semantic-release>
2026-05-23 22:01:23 +0000

    1.0.0
    Automatically generated by python-semantic-release

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# CHANGELOG
+
+
+## v1.0.0 (2026-05-23)
+
+- Initial Release
--- runradarrun/main.py
+++ runradarrun/main.py
@@ -3,7 +3,7 @@
 # code: language=python tabSize=4
 #
 
-__version__ = "0.2.0"
+__version__ = "1.0.0"
 
 
 import argparse
